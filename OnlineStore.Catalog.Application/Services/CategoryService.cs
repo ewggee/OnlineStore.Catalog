10b5f472a8c47c617262fab0c2f54f8809fdce10
Евгений Кузнецов <egions2006@gmail.com>
2025-05-06 20:22:55 +0300

    исправлен сервис категорий: добавлен выброс исключений

--- OnlineStore.Catalog.Application/Services/CategoryService.cs
+++ OnlineStore.Catalog.Application/Services/CategoryService.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using OnlineStore.Catalog.Application.Abstractions;
 using OnlineStore.Catalog.Contracts.Dtos;
+using OnlineStore.Catalog.Contracts.Helpers;
 using OnlineStore.Catalog.Domain.Entities;
+using OnlineStore.Catalog.Domain.Exceptions;
 using OnlineStore.Catalog.Domain.Repositories;
 
 namespace OnlineStore.Catalog.Application.Services;
@@ -9,22 +11,26 @@ namespace OnlineStore.Catalog.Application.Services;
 public class CategoryService : ICategoryService
 {
     private readonly ICategoryRepository _categoryRepository;
+    private readonly IProductRepository _productRepository;
     private readonly IMapper _mapper;
 
     public CategoryService(
         ICategoryRepository categoryRepository,
+        IProductRepository productRepository,
         IMapper mapper)
     {
         _categoryRepository = categoryRepository;
+        _productRepository = productRepository;
         _mapper = mapper;
     }
 
     /// <inheritdoc/>
     public async Task<CategoryDto> GetAsync(int categoryId, CancellationToken cancellation)
     {
-        var categoryDto = await _categoryRepository.GetAsync(categoryId);
+        var category = await _categoryRepository.GetAsync(categoryId, cancellation)
+            ?? throw new EntityNotFoundException(ExceptionMessagesHelper.EntityNotFound<Category>(categoryId));
 
-        return _mapper.Map<CategoryDto>(categoryDto);
+        return _mapper.Map<CategoryDto>(category);
     }
 
     /// <inheritdoc/>
@@ -36,11 +42,21 @@ public class CategoryService : ICategoryService
     }
 
     /// <inheritdoc/>
-    public async Task<IReadOnlyList<CategoryDto>> GetSubcategoriesByIdAsync(int categoryId, CancellationToken cancellation)
+    public async Task<CategoryWithSubcategoriesDto> GetCategoryWithSubcategoriesAsync(int categoryId, CancellationToken cancellation)
     {
-        var subCategories = await _categoryRepository.GetAllAsync(c => c.ParentCategoryId == categoryId, cancellation);
+        var category = await _categoryRepository.GetAsync(categoryId, cancellation) 
+            ?? throw new EntityNotFoundException(ExceptionMessagesHelper.EntityNotFound<Category>(categoryId));
 
-        return _mapper.Map<CategoryDto[]>(subCategories);
+        var subCategories = await _categoryRepository.GetAllAsync(c => c.ParentCategoryId == category.Id, cancellation);
+
+        var categoryDto = _mapper.Map<CategoryDto>(category);
+        var subCategoriesDtos = _mapper.Map<IReadOnlyList<CategoryDto>>(subCategories);
+
+        return new CategoryWithSubcategoriesDto
+        {
+            Category = categoryDto,
+            Subcategories = subCategoriesDtos
+        };
     }
 
     /// <inheritdoc/>
@@ -48,7 +64,7 @@ public class CategoryService : ICategoryService
     {
         var navigationCategories = await _categoryRepository.GetNavigationCategoriesAsync(cancellation, categoryId);
 
-        return _mapper.Map<CategoryDto[]>(navigationCategories);
+        return _mapper.Map<IReadOnlyList<CategoryDto>>(navigationCategories);
     }
 
     /// <inheritdoc/>
@@ -56,7 +72,7 @@ public class CategoryService : ICategoryService
     {
         var independentCategories = await _categoryRepository.GetWithoutSubsAsync(cancellation);
 
-        return _mapper.Map<CategoryDto[]>(independentCategories);
+        return _mapper.Map<IReadOnlyList<CategoryDto>>(independentCategories);
     }
 
     /// <inheritdoc/>
@@ -80,7 +96,8 @@ public class CategoryService : ICategoryService
     /// <inheritdoc/>
     public async Task DeleteAsync(int categoryId, CancellationToken cancellation)
     {
-        var category = new Category { Id = categoryId };
+        var category = await _categoryRepository.GetAsync(categoryId, cancellation)
+            ?? throw new EntityNotFoundException(ExceptionMessagesHelper.EntityNotFound<Category>(categoryId));
 
         await _categoryRepository.DeleteAsync(category, cancellation);
     }
